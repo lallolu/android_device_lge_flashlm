6a8826b0aa708bfeab1b68357dd8b8998e427483
Gabriel Adeyemi <gabriel.adeyemi@zenseact.com>
2021-08-11 16:19:39 +0200

    Adapt some things to generic devices.

--- extract-files.sh
+++ extract-files.sh
@@ -25,47 +25,58 @@ VENDOR=lge
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-. "$HELPER"
+source "$HELPER"
 
-function blob_fixup() {
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
     case "${1}" in
-    vendor/lib/hw/camera.msmnile.so)
-        patchelf --replace-needed "libc++.so" "libc++-v29.so" "${2}"
-        ;;
-    vendor/lib64/hw/camera.msmnile.so)
-        patchelf --replace-needed "libc++.so" "libc++-v29.so" "${2}"
-        ;;
-    esac
-}
-
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC=$1
-                                ;;
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
+
+#function blob_fixup() {
+#    case "${1}" in
+#    vendor/lib/hw/camera.msmnile.so)
+#        patchelf --replace-needed "libc++.so" "libc++-v29.so" "${2}"
+#        ;;
+#    vendor/lib64/hw/camera.msmnile.so)
+#        patchelf --replace-needed "libc++.so" "libc++-v29.so" "${2}"
+#        ;;
+#    esac
+#}
 
 if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "${MY_DIR}"/proprietary-files.txt "${SRC}" "${SECTION}"
 
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}"/setup-makefiles.sh
